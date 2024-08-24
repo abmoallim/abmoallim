0aa9fc5fe9f42a4a7982bc1bc83efad0fe9c38dd
Abdihamid Moallim <66555990+abmoallim@users.noreply.github.com>
2024-08-24 03:56:12 +0300

    post cover [web scraping ]

--- app/blog/page.js
+++ app/blog/page.js
@@ -5,9 +5,9 @@ const blogs = [
     {
         title: 'Web Scraping with Playwright and Node.js: A Practical Tutorial',
         slug: 'web-scraping-playwright-node',
-        image: '/imgs/project-3.png',
+        image: '/imgs/web-scraping.png',
         readingTime: '8 min read',
-        date: 'August 28, 2023',
+        date: 'August 24, 2024',
     },
     // Add more blog entries here
 ];
@@ -19,7 +19,11 @@ const Blog = () => {
                 <h2 className="text-4xl font-bold text-center text-gray-900 mb-8">Blog</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {blogs.map((blog, index) => (
-                        <div key={index} className="bg-white border rounded-lg shadow-md overflow-hidden transition-shadow hover:shadow-lg hover:shadow-gray-800">
+                        <a 
+                            key={index} 
+                            href={`/blog/${blog.slug}`} 
+                            className="block bg-white border rounded-lg shadow-md overflow-hidden transition-shadow hover:shadow-lg hover:shadow-gray-800"
+                        >
                             <img src={blog.image} alt={blog.title} className="w-full h-48 object-cover" />
                             <div className="p-4">
                                 <h3 className="text-2xl font-semibold text-gray-900 mb-2">{blog.title}</h3>
@@ -27,14 +31,8 @@ const Blog = () => {
                                     <span>{blog.readingTime}</span>
                                     <span>{blog.date}</span>
                                 </div>
-                                <a 
-                                    href={`/blog/${blog.slug}`} 
-                                    className="text-blue-500 hover:text-blue-700 underline"
-                                >
-                                    Read More
-                                </a>
                             </div>
-                        </div>
+                        </a>
                     ))}
                 </div>
             </div>
