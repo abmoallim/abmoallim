79e8b793c5ee3af46a4116f5b2f52b768e6568e6
Abdihamid Moallim <66555990+abmoallim@users.noreply.github.com>
2024-09-03 19:37:30 +0300

    load more blogs added

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 // app/blog/page.js
 
+'use client';
+import { useState } from 'react';
+
 const blogs = [
     {
         title: 'Web Scraping with Playwright and Node.js: A Practical Tutorial',
@@ -9,16 +12,25 @@ const blogs = [
         readingTime: '8 min read',
         date: 'August 24, 2024',
     },
+    
     // Add more blog entries here
 ];
 
+const ITEMS_PER_PAGE = 6;
+
 const Blog = () => {
+    const [visibleBlogs, setVisibleBlogs] = useState(ITEMS_PER_PAGE);
+
+    const loadMore = () => {
+        setVisibleBlogs(prevVisible => Math.min(prevVisible + ITEMS_PER_PAGE, blogs.length));
+    };
+
     return (
         <section id="blog" className="pt-32 py-16 bg-white dark:bg-gray-800 text-black dark:text-white">
             <div className="container mx-auto px-6 md:px-12 lg:px-24">
                 <h2 className="text-4xl font-bold text-center text-gray-900 dark:text-slate-400 mb-8">Blog</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {blogs.map((blog, index) => (
+                    {blogs.slice(0, visibleBlogs).map((blog, index) => (
                         <a 
                             key={index} 
                             href={`/blog/${blog.slug}`} 
@@ -35,6 +47,16 @@ const Blog = () => {
                         </a>
                     ))}
                 </div>
+                {visibleBlogs < blogs.length && (
+                    <div className="text-center mt-8">
+                        <button 
+                            onClick={loadMore}
+                            className="bg-slate-200 hover:bg-slate-300 text-slate-900 font-bold py-2 px-4 rounded-full"
+                        >
+                            Load More
+                        </button>
+                    </div>
+                )}
             </div>
         </section>
     );
