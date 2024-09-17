dbf9c0f5b8524bf0db66a10a038e4abef7edbe48
Abdihamid Moallim <66555990+abmoallim@users.noreply.github.com>
2024-09-17 22:26:45 +0300

    Technologies

--- app/components/Experience.js
+++ app/components/Experience.js
@@ -8,10 +8,10 @@ const Experience = () => {
     return (
         <section id="experience" className="py-16 bg-white dark:bg-gray-900 text-black dark:text-white">
             <div className="container mx-auto px-6 md:px-12 lg:px-24">
-                <h2 className="text-4xl font-bold text-center text-gray-900 mb-8">Experience</h2>
+                <h2 className="text-4xl font-bold text-center text-gray-900 mb-8">Technologies</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     <div className="p-6 border rounded-lg shadow-md bg-gray-50 dark:bg-slate-800">
-                        <h3 className="text-2xl font-semibold text-center mb-4">Frontend Development</h3>
+                        {/* <h3 className="text-2xl font-semibold text-center mb-4">Frontend Development</h3> */}
                         <ul className="space-y-4">
                             <li><FontAwesomeIcon icon={faHtml5} className="text-orange-500 w-6 h-6 mr-2" /><strong>HTML</strong> - Experienced</li>
                             <li><FontAwesomeIcon icon={faCss3Alt} className="text-blue-500 w-6 h-6 mr-2" /><strong>CSS</strong> - Experienced</li>
@@ -23,7 +23,7 @@ const Experience = () => {
                         </ul>
                     </div>
                     <div className="p-6 border rounded-lg shadow-md bg-gray-50 dark:bg-slate-800">
-                        <h3 className="text-2xl font-semibold text-center mb-4">Back-end Development</h3>
+                        {/* <h3 className="text-2xl font-semibold text-center mb-4">Back-end Development</h3> */}
                         <ul className="space-y-4">
                             <li><FontAwesomeIcon icon={faNodeJs} className="text-green-500 w-6 h-6 mr-2" /><strong>Node.js</strong> - Advanced</li>
                             <li><FontAwesomeIcon icon={faPython} className="text-blue-500 w-6 h-6 mr-2" /><strong>Python</strong> - Experienced</li>
