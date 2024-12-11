bb0b36828f15a71c948b56645d3a4807ecdfe435
Abdihamid Moallim <66555990+abmoallim@users.noreply.github.com>
2024-12-11 15:14:25 +0300

    google analytics added

--- app/layout.js
+++ app/layout.js
@@ -4,18 +4,33 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-import Header from './components/Header';
-import FloatingIcons from './components/FloatingIcons';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import FloatingIcons from "./components/FloatingIcons";
+import Footer from "./components/Footer";
+import Script from "next/script";
 
 export const metadata = {
-  title: 'abmoallim',
-  description: 'Personal Portfolio of Abdihamid Moallim',
+  title: "abmoallim",
+  description: "Personal Portfolio of Abdihamid Moallim",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZBHQKS3HR1"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZBHQKS3HR1');
+          `}
+        </Script>
+      </head>
       <body>
         <Header />
         {children}
